8f34dae3287bee5c39255e9283b84cd3c10ef85c
picolloo <lucaspicolloo@gmail.com>
2021-09-16 20:47:07 -0300

    connect to mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 async function connect() {
-  await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`).catch(e => {
-    console.error(e)
+  await mongoose.connect(
+    `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,
+  ).catch((e) => {
+    console.error(e);
     process.exit(1);
-  })
+  });
 }
-
 
 export default {
-  connect
-}
+  connect,
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import Express from 'express';
 import { config as dotenv } from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 
 import router from './router.js';
 import db from './database/index.js';
 
-dotenv();
 (async () => {
-  await db.connect()
+  dotenv();
+  await db.connect();
   const app = Express();
 
-  app.use(cors())
-  app.use(router)
+  app.use(cors());
+  app.use(router);
   app.listen(3000, () => console.log('server bound to port 3000'));
-})()
-
+})();
